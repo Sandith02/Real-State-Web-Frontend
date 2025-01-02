26453fa24c9f0c1c72f090e8487abc1ed7f1cefa
Sandith02 <lhthenuwara@gmail.com>
2025-01-02 18:31:33 +0530

    Picture Update

--- web-frontend/src/Pages/HomePage.jsx
+++ web-frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import VillaImage from '../Assets/Villa02.jpeg';  // Import the image
 
 const HomePage = () => {
   return (
-    <div>
+    <div 
+      style={{
+        backgroundImage: `url(${VillaImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',  // Full viewport height
+      }}
+    >
+      <h1>Welcome to Our Homepage</h1>
+      <p>This is where the main content of your application will be displayed.</p>
     </div>
-  )
-}
-export default HomePage
+  );
+};
+const StyledWrapper = styled.div`
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+   }
+`;
+
+export default HomePage;
+
+
